b4b249102bac8336492f947bbddcf60b202b21d2
Mạnh <manhtran18001008@gmail.com>
2025-09-09 10:58:39 +0700

    postController

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\PostController;
 
 Route::group([
     'middlware' => 'api',
@@ -15,4 +16,16 @@ Route::group([
     Route::post('/profile', [AuthController::class, 'profile'])->middleware('auth:api');
     Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
     Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.reset');
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'posts',
+], function ($router) {
+    Route::get('/', [PostController::class, 'index']);
+    Route::get('/all', [PostController::class, 'showAll']);
+    Route::post('/', [PostController::class, 'store']);
+    Route::get('/{id}', [PostController::class, 'show']);
+    Route::put('/{id}', [PostController::class, 'update']);
+    Route::delete('/{id}', [PostController::class, 'destroy']);
 });
